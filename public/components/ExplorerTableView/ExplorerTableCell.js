d28d49e7a6d45bc064469870c3486409fa6920fd
Ian Grail <grailians@gmail.com>
2016-04-24 19:32:42 -0400

    chore: remove some debugging logs

--- public/components/ExplorerTableView/ExplorerTableCell.js
+++ public/components/ExplorerTableView/ExplorerTableCell.js
@@ -3,19 +3,14 @@ import { RIEInput } from 'riek'
 import JSONTree from 'react-json-tree';
 import _ from 'lodash';
 
-console.log("heeeyyyyy")
-
 var ExplorerTableCell = React.createClass({
   dataChanged: function(data) {
     this.props.rowChanged(_.merge({}, this.props.row, data), this.props.fieldName);
   },
   composeCellBody: function(row, fieldName) {
-    // const data = JSON.parse(JSON.stringify(row[fieldName]));
+
     const data = row[fieldName];
-    if (this.props.row.id === '965c8bd9-ba13-4448-a4c9-be258bdc182d' && this.props.fieldName === 'name') {
-      console.log("data", data)
-    }
-    // data = new Date().getSeconds();
+
     if (typeof data !== 'object') {
       return <RIEInput value={data} change={this.dataChanged} propName={fieldName} shouldBlockWhileLoading={true} classLoading="loading-cell" classEditing="form-control" />;
     } else {
@@ -31,10 +26,6 @@ var ExplorerTableCell = React.createClass({
     }
   },
   render: function() {
-    if (this.props.row.id === '965c8bd9-ba13-4448-a4c9-be258bdc182d' && this.props.fieldName === 'name') {
-      console.log("render TableCell")
-      console.log("this.props.row", this.props.row)
-    }
     return (
       <div>
         {this.composeCellBody(this.props.row, this.props.fieldName)}
