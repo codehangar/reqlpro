f40ecbb2437b488028f40df8d424a463511797f6
Ian Grail <grailians@gmail.com>
2016-04-24 20:53:40 -0400

    feat: make columns resizable

--- public/components/ExplorerTableView/ExplorerTableView.js
+++ public/components/ExplorerTableView/ExplorerTableView.js
@@ -1,11 +1,39 @@
 const React = require('react');
 const {Table, Column, Cell} = require('fixed-data-table');
-
 import JSONTree from 'react-json-tree';
-import ExplorerTableCell from './ExplorerTableCell.js'
+import ExplorerTableCell from './ExplorerTableCell.js';
+import _ from 'lodash';
 var RethinkDbClient = window.rethinkDbClient;
 
 var ExplorerTableView = React.createClass({
+  getInitialState: function() {
+
+    let maximumProps = 0;
+    let rowIndexOfMaximum = 0;
+    this.props.table.data.map(function(row, index) {
+      maximumProps = Object.keys(row).length > maximumProps ? Object.keys(row).length : maximumProps;
+      rowIndexOfMaximum = Object.keys(row).length > maximumProps ? index : rowIndexOfMaximum;
+    });
+
+    let columnWidths = {};
+    Object.keys(this.props.table.data[rowIndexOfMaximum]).map((fieldName, index) => {
+      columnWidths[fieldName] = 200;
+    });
+
+    return {
+      columnWidths: columnWidths
+    };
+  },
+  componentWillReceiveProps: function() {
+    console.log(" -> ExplorerTableView componentWillReceiveProps this.props", this.props)
+  },
+  _onColumnResizeEndCallback: function (newColumnWidth, columnKey) {
+    var width= {};
+    width[columnKey] = newColumnWidth;
+    this.setState(({columnWidths}) => ({
+      columnWidths: _.extend({}, columnWidths, width)
+    }));
+  },
   rowChanged: function (row) {
     console.log("rowChanged row", row)
     RethinkDbClient.update(row);
@@ -27,6 +55,8 @@ var ExplorerTableView = React.createClass({
         <Column
           key={index}
           header={<Cell>{fieldName}</Cell>}
+          isResizable={true}
+          columnKey={fieldName}
           cell={(props) => {
                 if (this.props.table.data[props.rowIndex].id === '965c8bd9-ba13-4448-a4c9-be258bdc182d' && fieldName === 'name') {
                   console.log("   --> ExplorerTableView cell render", this.props.table.data[props.rowIndex][fieldName])
@@ -38,7 +68,7 @@ var ExplorerTableView = React.createClass({
                 );
               }
           }
-          width={200} />
+          width={this.state.columnWidths[fieldName]} />
       );
     });
     return (
@@ -46,6 +76,8 @@ var ExplorerTableView = React.createClass({
         rowsCount={this.props.table.data.length}
         rowHeight={50}
         headerHeight={30}
+        onColumnResizeEndCallback={this._onColumnResizeEndCallback}
+        isColumnResizing={false}
         width={document.getElementById('explorer-body').offsetWidth}
         height={window.innerHeight - 100}>
         {columnNodes}
