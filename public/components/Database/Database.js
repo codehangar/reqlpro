1875341e8800836e4b8cdab7fe8c055f0c73f6a5
Keith Keough <keithakeough@gmail.com>
2016-06-12 15:41:39 -0400

    hide edit database button for now until feature is done

--- public/components/Database/Database.js
+++ public/components/Database/Database.js
@@ -33,7 +33,7 @@ const Database = React.createClass({
             &nbsp;&nbsp;<span className="database-name">{this.props.database.name}</span>
             <div className="btn-group" role="group">
               <button onClick={this.deleteDatabase.bind(this, this.props.database.name)} className="btn btn-default fa fa-trash"></button>
-              <button onClick={this.editDatabase.bind(this, this.props.database.name)} className="btn btn-default fa fa-pencil"></button>
+              {/*<button onClick={this.editDatabase.bind(this, this.props.database.name)} className="btn btn-default fa fa-pencil"></button>*/}
             </div>
           </div>
         {dbTables}
