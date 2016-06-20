b1debb276bc03f890408f9757cbe193c08700cb8
Cassandra Wilcox <csndra0504@gmail.com>
2016-06-20 18:45:12 -0400

    style: update connection dropdown to use bootstrap dropdown-menu classes

--- public/components/DatabasesHeader/DatabasesHeader.js
+++ public/components/DatabasesHeader/DatabasesHeader.js
@@ -15,9 +15,7 @@ const DatabasesHeader = React.createClass({
   render: function() {
     var classes = {
       actionMenu: classNames(
-        'connection-action-menu',
-        'panel',
-        'panel-default',
+        'dropdown-menu',
         {
           'show': this.props.store.router.ConnectionActionMenu.show,
           'hidden': !this.props.store.router.ConnectionActionMenu.show
@@ -26,11 +24,15 @@ const DatabasesHeader = React.createClass({
     }
     return (
       <div className="databases-header">
-        <div>{this.props.selectedFavorite.name} <i onClick={this.toggleConnectionActionMenu} className="fa fa-bars connection-action-menu-button"></i></div>
-        <ul className={classes.actionMenu}>
-          <li onClick={this.editFavorite}>Edit Connection</li>
-          <li onClick={this.addDatabase}>Add database</li>
-        </ul>
+        <div><span className="title">{this.props.selectedFavorite.name}</span>
+          <div className="dropdown pull-right">
+            <i onClick={this.toggleConnectionActionMenu} className="fa fa-bars connection-action-menu-button"></i>
+            <ul className={classes.actionMenu}>
+              <li onClick={this.editFavorite}><a href="#">Edit Connection</a></li>
+              <li onClick={this.addDatabase}><a href="#">Add database</a></li>
+            </ul>
+          </div>  
+        </div>
       </div>
     );
   }
