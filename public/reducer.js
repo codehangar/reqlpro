f1be8ebef80e20729709c02c7151ec7fe83034e8
Cassandra Wilcox <csndra0504@gmail.com>
2016-09-04 20:14:42 -0400

    feat: reducer handles HIDE_CONNECTION_FORM

--- public/reducer.js
+++ public/reducer.js
@@ -4,11 +4,11 @@ import {
   setConnections,
   setEmail,
   addConnection,
-  showConnectionForm
+  showConnectionForm,
+  hideConnectionForm
 } from './core';
 
 export default function reducer(state = Map(), action) {
-  // console.log('from reducer', state, action)
   switch (action.type) {
     case 'SET_STATE':
       return setState(state, action.state);
@@ -17,11 +17,11 @@ export default function reducer(state = Map(), action) {
     case 'SET_EMAIL':
       return setEmail(state, action.email);
     case 'ADD_CONNECTION':
-      console.log('ADD_CONNECTION', state, action.connection);
       return addConnection(state, action.connection);
     case 'SHOW_CONNECTION_FORM':
-      console.log('SHOW_CONNECTION_FORM', state, action.mode);
       return showConnectionForm(state, action.mode);
+    case 'HIDE_CONNECTION_FORM':
+      return hideConnectionForm(state);
   }
   return state;
 }
