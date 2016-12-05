755750899cf442ce341ba7f5583ebb3aec4b816e
Cassandra Wilcox <csndra0504@gmail.com>
2016-12-05 14:26:30 -0500

    feat: call RethinkDbService to getDbList, dispatch event to reducer when complete

--- public/actions.js
+++ public/actions.js
@@ -3,13 +3,36 @@ import RethinkDbService from '../main/services/rethinkdb.service';
 export function getDbConnection(connection) {
   return dispatch => {
     new Promise((resolve, reject) => {
-      console.log('connection', connection);
+      // console.log('connection', connection);
       RethinkDbService.getConnection(connection.host, connection.port, connection.authKey).then((conn) => {
         console.log('getConnection conn', conn);
+
         dispatch({
           type: 'SET_DB_CONNECTION',
           dbConnection: conn
         });
+
+        RethinkDbService.getDbList(conn).then(function(dblist) {
+          var databases = [];
+          for (var i = 0; i < dblist.length; i++) {
+            databases.push({
+              name: dblist[i],
+              tables: []
+            });
+          }
+          console.log('getDbList success, databases', databases)
+          dispatch({
+            type: 'SET_DB_LIST',
+            databases: databases
+          });
+        }).catch(function(err) {
+          console.log('getDbList error', err)
+          dispatch({
+            type: 'SET_DB_LIST',
+            databases: err
+          });
+        });
+
         resolve(conn);
       }).catch(error => {
         dispatch({
