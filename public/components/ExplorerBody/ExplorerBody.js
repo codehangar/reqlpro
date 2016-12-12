d33d07f9f882a6f7f829a38030d2b66c54934a09
Cassandra Wilcox <csndra0504@gmail.com>
2016-12-11 19:12:47 -0500

    refactor: ExplorerBody component

--- public/components/ExplorerBody/ExplorerBody.js
+++ public/components/ExplorerBody/ExplorerBody.js
@@ -1,38 +1,39 @@
-const React = require('react');
-const ExplorerTreeView = require('../ExplorerTreeView/ExplorerTreeView');
-const ExplorerTableView = require('../ExplorerTableView/ExplorerTableView');
-const ExplorerCodeView = require('../ExplorerCodeView/ExplorerCodeView');
+import React from 'react';
+import ExplorerTreeView from '../ExplorerTreeView/ExplorerTreeView';
+import ExplorerTableView from '../ExplorerTableView/ExplorerTableView';
+import ExplorerCodeView from '../ExplorerCodeView/ExplorerCodeView';
 
-const ExplorerBody = React.createClass({
-  render: function() {
+const ExplorerBody = ({
+  width,
+  table
+}) => {
     let explorerBody;
-    // console.log(" **** ExplorerBody this.props", this.props)
-    if (this.props.table.loading) {
+    if (table.loading) {
       explorerBody = (
         <div className="explorer-loading">
            <span className="fa fa-refresh fa-spin"></span>
         </div>
       );
     } else {
-      if (this.props.table.data.length || this.props.table.type === 'code') {
-        if (this.props.table.type === 'tree') {
+      if (table.data.length || table.type === 'code') {
+        if (table.type === 'tree') {
           explorerBody = (
             <div>
-              <ExplorerTreeView table={this.props.table} />
+              <ExplorerTreeView table={table} />
             </div>
           );
-        } else if (this.props.table.type === 'table') {
+        } else if (table.type === 'table') {
           explorerBody = (
             <div style={{position:'relative'}}>
               <div className="table-view-container">
-                <ExplorerTableView table={this.props.table} className="data-table-main"/>
+                <ExplorerTableView table={table} className="data-table-main"/>
               </div>
             </div>
           );
-        } else if(this.props.table.type === 'code') {
+        } else if(table.type === 'code') {
           explorerBody = (
             <div>
-              <ExplorerCodeView table={this.props.table} />
+              <ExplorerCodeView table={table} />
             </div>
           )
         }
@@ -49,15 +50,9 @@ const ExplorerBody = React.createClass({
         );
       }
     }
-
-    return (
-      <div className="explorer-body" style={{
-        width: this.props.width
-      }}>
-          {explorerBody}
-      </div>
-    );
-  }
-});
+  return (
+    <div className="explorer-body" style={{width: width}}>{explorerBody}</div>
+  );
+};
 
 module.exports = ExplorerBody;
