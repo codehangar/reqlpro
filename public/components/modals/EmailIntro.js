1d9aabcebf64373f7d88640ed21f5a2ff573f27f
Ian Grail <grailians@gmail.com>
2017-01-20 06:15:31 +0700

    chore(emailForm): Allow Enter to submit

--- public/components/modals/EmailIntro.js
+++ public/components/modals/EmailIntro.js
@@ -1,14 +1,18 @@
 const React = require('react');
-const classNames = require('classnames');
 const Segment = require('../../services/segment.service.js');
-import {connect} from 'react-redux';
-import {writeConfigFile} from '../../actions';
+import { connect } from 'react-redux';
+import { writeConfigFile } from '../../actions';
 
 const ConnectionForm = ({
   email,
   onEmailSubmit
 }) => {
-  let input;
+  let nameInput;
+
+  const submit = (e) => {
+    e.preventDefault();
+    onEmailSubmit(nameInput.value)
+  };
 
   const containerStyles = {
     display: !email ? 'block' : 'none'
@@ -24,17 +28,17 @@ const ConnectionForm = ({
               <p className="lead">Thank you for downloading ReQLPro.</p>
               <p> To continue, please register for free by providing your email below.</p>
               <div className="row">
-                <form>
+                <form onSubmit={submit}>
                   <div className="col-xs-10 col-xs-offset-1 text-center">
                     <div className="input-group">
-                      <input type="text" className="form-control" id="email" placeholder="Email"
-                             ref={node => {
-                               input = node;
-                             }}/>
+                      <input type="text" className="form-control" id="email" placeholder="Email" ref={(input) => {
+                        nameInput = input;
+                        if (nameInput) {
+                          nameInput.focus();
+                        }
+                      }}/>
                       <div className="input-group-btn">
-                        <button className="btn btn-primary" type="button" onClick={() => {
-                          onEmailSubmit(input.value)
-                        }}>
+                        <button className="btn btn-primary" type="submit">
                           Submit
                         </button>
                       </div>
@@ -42,10 +46,12 @@ const ConnectionForm = ({
                   </div>
                 </form>
               </div>
-              <p className="email-built-by">ReQLPro was built with <i className="fa fa-heart"/> by <a href="http://www.codehangar.io" target="_blank">Code Hangar, Inc.</a></p>
+              <p className="email-built-by">ReQLPro was built with <i className="fa fa-heart"/> by <a
+                href="http://www.codehangar.io" target="_blank">Code Hangar, Inc.</a></p>
             </div>
             <div className="panel-footer text-center">
-              Have a question? Visit our <a href="http://utils.codehangar.io/rethink/support" target="_blank">Help Center</a> or <a className="cursor-pointer" onClick={function() {
+              Have a question? Visit our <a href="http://utils.codehangar.io/rethink/support" target="_blank">Help
+              Center</a> or <a className="cursor-pointer" onClick={function() {
               HS.beacon.open();
             }}>send us a message</a>.
             </div>
