2ed93d753a8aafbb7686541fa76452d716626a57
es50678 <es50678@gmail.com>
2017-01-24 11:20:59 -0500

    fix: make "npm start" work on windows

--- tools/start.js
+++ tools/start.js
@@ -7,11 +7,11 @@ var spawn = require('child_process').spawn;
 // child.stdout.pipe(process.stdout);
 // child.stdout.on('data', function(chunk) {
 
-  var child2 = spawn('electron', ['main.js', '--dev']);
+  var child2 = spawn('electron', ['main.js', '--dev'], { shell: true });
   child2.stdout.pipe(process.stdout);
   child2.stderr.pipe(process.stderr);
 
-  var child3 = spawn('webpack', ['--colors', '--watch']);
+  var child3 = spawn('webpack', ['--colors', '--watch'], { shell: true });
   child3.stdout.pipe(process.stdout);
   child3.stderr.pipe(process.stderr);
 // });
