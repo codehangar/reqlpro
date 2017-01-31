fb5998ffecbf8fc17978eaa2277e346d67e8d4e7
Eduardo <edsoto@codehangar.io>
2017-01-31 14:30:15 -0500

    fix: focuses the app on newly added table

--- public/components/Sidebar/Databases/Database/DbTables/tables.actions.js
+++ public/components/Sidebar/Databases/Database/DbTables/tables.actions.js
@@ -1,5 +1,6 @@
-const RethinkDbService = require('../../../../../services/rethinkdb.service.js');
+const RethinkDbService = require('../../../../../services/rethinkdb.service');
 import * as types from '../../../../../action-types';
+import { queryTable } from '../../../../../actions';
 
 export function getDbTables(dbConnection, dbName) {
   return dispatch => {
@@ -21,20 +22,24 @@ export function getDbTables(dbConnection, dbName) {
 export function createTable(dbConnection, dbName, tableName) {
   return dispatch => {
     return new Promise((resolve, reject) => {
-      dispatch({
-        type: 'SET_CONNECTION_LOADING',
-        loading: true
-      });
+
       RethinkDbService.createTable(dbConnection, dbName, tableName, 'id').then((results) => {
         dispatch({
           type: types.ADD_TO_TABLE_LIST,
           dbName,
           tableName
         });
-        dispatch({
-          type: 'SET_CONNECTION_LOADING',
-          loading: false
+        const selectedTable = dispatch({
+          type: 'SET_SELECTED_TABLE',
+          dbName,
+          tableName
         });
+        if (selectedTable){
+          dispatch(queryTable(dbConnection, dbName, tableName, selectedTable.query));
+        } else {
+          dispatch(queryTable(dbConnection, dbName, tableName));
+        }
+
         resolve(results);
       }).catch(error => {
         dispatch({
