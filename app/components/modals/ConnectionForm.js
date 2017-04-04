1d1d95cdf33c2de1aa46309e9bb955549597e5e3
Ian Grail <grailians@gmail.com>
2017-04-04 18:58:34 +0700

    feat: Add Support for SSL connections

--- app/components/modals/ConnectionForm.js
+++ app/components/modals/ConnectionForm.js
@@ -21,7 +21,7 @@ class ConnectionForm extends Component {
   render() {
     const {
       selectedConnection,
-      showConnectionForm = true, 
+      showConnectionForm = true,
       isAdd,
       onCancel,
       onDelete,
@@ -33,93 +33,110 @@ class ConnectionForm extends Component {
     const isRequired = (val) => val && val.length > 0;
     let valStateUser = null;
     let valStatePass = null;
-  
-    if(connectionError){
-      if(connectionError.error.msg == 'Wrong password'){
-        valStatePass = "error"; 
-      } else if( connectionError.error.msg == 'Unknown user') {
+
+    if (connectionError) {
+      if (connectionError.error.msg === 'Wrong password') {
+        valStatePass = "error";
+      } else if (connectionError.error.msg === 'Unknown user') {
         valStateUser = "error"
       }
     }
 
     return (
       <Modal show={showConnectionForm} onHide={onCancel}>
-        <Form model="forms.connection" validators={{ name : {isRequired} }} onSubmit={(data) => isAdd ? onSave(data) : onUpdate(data)}>
+        <Form model="forms.connection" validators={{ name: { isRequired } }}
+              onSubmit={(data) => isAdd ? onSave(data) : onUpdate(data)}>
           <Modal.Header closeButton>
             <Modal.Title>{isAdd ? 'Add New' : 'Edit'} RethinkDB Connection</Modal.Title>
-          </Modal.Header>  
+          </Modal.Header>
           <Modal.Body>
-              <div className="panel-body">
-                <div className="row">
-                  <div className="col-sm-12">
-                    <div className="connection-row">
-                      <label>Connection Name</label>
-                      <Field model=".name" type="name">
-                        <input className="form-control" type="text" id="name"
-                          ref={(input) => {
-                            this.nameInput = input;
-                          } }
-                          placeholder="i.e. TodoApp-local" />
-                      <Errors
-                        className="errors"
-                        wrapper="span"
-                        show="touched"
-                        model=".name"
-                        messages={{
-                          isRequired: 'Please provide a connection name.',
-                        }}
-                      />
-                      </Field>
-                    </div>
-                    <div className="connection-row">
-                      <label>Host</label>
-                      <Field model=".host">
-                        <input className="form-control" type="text" id="host"
-                          placeholder="i.e. localhost" />
-                          <small className="text-muted">
-                            By default, RethinkDB binds to <strong>localhost</strong> if host is not specified.
-                          </small>
-                      </Field>
-                    </div>
-                    <div className="connection-row">
-                      <label>Port</label>
-                      <Field model=".port">
-                        <input className="form-control" type="text" id="host"
-                          placeholder="i.e. 28015" />
-                           <small className="text-muted">
-                            By default, RethinkDB binds to port <strong>28015</strong> if port is not specified.
-                          </small>
-                    </Field>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-6">
-                      <label>User</label>
-                      <Field model=".user">
-                        <FormGroup validationState={valStateUser}>
-                          <input className="form-control" type="text" id="user" />
-                        </FormGroup>
-                      </Field>
-                  </div>
-                  <div className="col-sm-6">
-                      <label>Pass</label>
-                      <Field model=".pass">
-                        <FormGroup  validationState={valStatePass}>
-                          <input className="form-control" type="password" id="pass"/>
-                        </FormGroup>
-                      </Field>
-                    </div>
-                  </div>
+            <div className="panel-body">
+              <div className="connection-row">
+                <label>Connection Name</label>
+                <Field model=".name" type="name">
+                  <input className="form-control" type="text" id="name"
+                         ref={(input) => {
+                           this.nameInput = input;
+                         } }
+                         placeholder="i.e. TodoApp-local"/>
+                  <Errors
+                    className="errors"
+                    wrapper="span"
+                    show="touched"
+                    model=".name"
+                    messages={{
+                      isRequired: 'Please provide a connection name.',
+                    }}
+                  />
+                </Field>
+              </div>
+              <div className="row">
+                <div className="col-sm-6">
+                  <label>Host</label>
+                  <Field model=".host">
+                    <input className="form-control" type="text" id="host"
+                           placeholder="i.e. localhost"/>
+                    <small className="text-muted">
+                      By default, RethinkDB binds to <strong>localhost</strong> if host is not specified.
+                    </small>
+                  </Field>
                 </div>
+                <div className="col-sm-6">
+                  <label>Port</label>
+                  <Field model=".port">
+                    <input className="form-control" type="text" id="host"
+                           placeholder="i.e. 28015"/>
+                    <small className="text-muted">
+                      By default, RethinkDB binds to port <strong>28015</strong> if port is not specified.
+                    </small>
+                  </Field>
+                </div>
+              </div>
+              <hr/>
+              <div className="row">
+                <div className="col-sm-6">
+                  <label>User</label>
+                  <Field model=".user">
+                    <FormGroup validationState={valStateUser}>
+                      <input className="form-control" type="text" id="user"/>
+                    </FormGroup>
+                  </Field>
+                </div>
+                <div className="col-sm-6">
+                  <label>Pass</label>
+                  <Field model=".pass">
+                    <FormGroup validationState={valStatePass}>
+                      <input className="form-control" type="password" id="pass"/>
+                    </FormGroup>
+                  </Field>
+                </div>
+              </div>
+              <hr/>
+              <div className="connection-row">
+                <label>SSL Certificate</label>
+                <Field model=".ca">
+                  <FormGroup validationState={valStateUser}>
+                    <textarea className="form-control" id="ca"/>
+                  </FormGroup>
+                </Field>
+              </div>
+              <div className="connection-row">
+                <Field model=".rejectUnauthorized">
+                  <FormGroup validationState={valStatePass}>
+                    <input type="checkbox" id="rejectUnauthorized"/>
+                    <label htmlFor="rejectUnauthorized" style={{ marginLeft: '10px' }}>SSL Reject Unauthorized</label>
+                  </FormGroup>
+                </Field>
               </div>
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <button type="submit" className="btn btn-primary pull-right">Save</button> 
+            <button type="submit" className="btn btn-primary pull-right">Save</button>
             <button type="button" onClick={onCancel} className="btn btn-default pull-left">Cancel</button>
             {!isAdd ?
               <button type="button" className="btn btn-default" onClick={() => onDelete(selectedConnection)}>
-                Delete</button> : ''} 
-            <div className="clearfix" />
+                Delete</button> : ''}
+            <div className="clearfix"/>
           </Modal.Footer>
         </Form>
       </Modal>
@@ -148,8 +165,9 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(addConnection(data));
       Segment.track({
         event: 'Add Connection',
-        properties:{
-          'Uses Permissions': !!data.user
+        properties: {
+          'Uses Permissions': !!data.user,
+          'Uses SSL': !!data.ca
         }
       });
     },
@@ -157,8 +175,9 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(updateConnection(data));
       Segment.track({
         event: 'Update Connection',
-        properties:{
-          'Uses Permissions': !!data.user
+        properties: {
+          'Uses Permissions': !!data.user,
+          'Uses SSL': !!data.ca
         }
       });
     },
