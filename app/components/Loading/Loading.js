fc8f67738ce52a72f74f116ac7f0a5707f32fedb
Cassandra Wilcox <cassie@codehangar.io>
2017-10-02 22:47:25 -0400

    refactor: move Loading markup from Explorer component to its own Loading component

---
+++ app/components/Loading/Loading.js
@@ -0,0 +1,12 @@
+const Loading = ({}) => {
+  return (
+    <div className="explorer-container">
+      <div className="explorer-loading">
+        <span className="fa fa-refresh fa-spin"/>
+        <p>Loading...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
